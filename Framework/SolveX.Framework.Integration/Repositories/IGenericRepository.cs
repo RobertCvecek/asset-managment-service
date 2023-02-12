e156afeb4a1d293a73a7bab11f6f58d64d8bebcd
Robert_Cvecek <robi.cvecek1@gmail.com>
2023-02-12 17:52:44 +0100

    feat(framework-integration): Generic repository and entity for EF Core

---
+++ Framework/SolveX.Framework.Integration/Repositories/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace SolveX.Framework.Integration.Repositories;
+public interface IGenericRepository<T>
+{
+    public Task<T> GetAsync(int id);
+
+    public IQueryable<T> Query();
+
+    public Task<int> InsertAsync(T entity);
+
+    public Task UpdateAsync(T entity);
+
+    public Task DeleteAsync(int id);
+}
