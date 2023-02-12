0684854688bd27c7b0243eeefd31cf5ddb0c2adb
Robert_Cvecek <robi.cvecek1@gmail.com>
2023-02-12 17:53:37 +0100

    feat(framewor-domain): Exception for missing data

---
+++ Framework/SolveX.Framework.Domain/Exceptions/DataNotFoundException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SolveX.Framework.Domain.Exceptions;
+
+public class DataNotFoundException : Exception
+{
+    public DataNotFoundException() : base() { }
+
+    public DataNotFoundException(string message) : base(message) { }
+
+    public DataNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+
+
+    public DataNotFoundException(int errorCode) : this()
+    {
+        ErrorCode = errorCode;
+    }
+
+    public DataNotFoundException(int errorCode, string message) : this(message)
+    {
+        ErrorCode = errorCode;
+    }
+
+    public DataNotFoundException(int errorCode, string message, Exception innerException) : this(message, innerException)
+    {
+        ErrorCode = errorCode;
+    }
+
+    /// <summary>
+    /// Business error code
+    /// </summary>
+    public int ErrorCode { get; private set; }
+}
