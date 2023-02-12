465ddf1065dc6e51f040089ba6b3ae47d92bf14f
Robert_Cvecek <robi.cvecek1@gmail.com>
2023-02-12 17:55:05 +0100

    feat(business-user-domain): Domain service and models for users

---
+++ Business/Users/SolveX.Business.Users.Domain/DomainServices/IUserDomainService.cs
@@ -0,0 +1,10 @@
+﻿using SolveX.Business.Users.Domain.Models;
+
+namespace SolveX.Business.Users.Domain.DomainServices;
+
+public interface IUserDomainService
+{
+    public Task<UserModel> Login(string username, string password);
+
+    public Task<int> Register(UserModel value, bool isAdmin);
+}
---
+++ Business/Users/SolveX.Business.Users.Domain/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using SolveX.Framework.Integration.Repositories.Models;
+
+namespace SolveX.Business.Users.Domain.Models;
+
+public class UserModel : BaseEntity
+{
+    public string UserName { get; set; }
+
+    public string Password { get; set; }
+
+    public string Role { get; set; }
+}
