2eb4b18078f30b6a019f0a630a2d2d85994f04b7
Robert_Cvecek <robi.cvecek1@gmail.com>
2023-02-12 17:52:44 +0100

    feat(framework-integration): Generic repository and entity for EF Core

---
+++ Framework/SolveX.Framework.Integration/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolveX.Framework.Integration.Repositories.Models;
+
+namespace SolveX.Framework.Integration.Repositories;
+
+    public abstract class GenericRepository<T> : IGenericRepository<T>
+       where T : BaseEntity, new()
+    {
+        protected DbContext _dbContext { get; set; }
+
+        public GenericRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<T> GetAsync(int id)
+        {
+            return await _dbContext.FindAsync<T>(id);
+        }
+
+        public IQueryable<T> Query()
+        {
+            return _dbContext.Set<T>().AsQueryable();
+        }
+
+        public async Task<int> InsertAsync(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            T entity = new T() { Id = id };
+
+            _dbContext.Entry(entity).State = EntityState.Deleted;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+
