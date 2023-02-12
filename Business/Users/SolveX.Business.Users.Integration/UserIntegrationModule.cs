c84c10b85041baa28a3f538161f251457573dd6e
Robert_Cvecek <robi.cvecek1@gmail.com>
2023-02-12 17:54:22 +0100

    feat(business-users-integration) Migrations, context and repository for user data

---
+++ Business/Users/SolveX.Business.Users.Integration/UserIntegrationModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using SolveX.Business.Users.Domain.Repositories;
+using SolveX.Business.Users.Integration.Context;
+using SolveX.Business.Users.Integration.Repositories;
+
+namespace SolveX.Business.Users.Integration;
+
+public class UserIntegrationModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<UserContext>();
+        builder.RegisterType<UserRepository>().As<IUserRepository>();
+    }
+}
