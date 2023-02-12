104a9a104031228479a8b220caac60a9d74c344a
Robert_Cvecek <robi.cvecek1@gmail.com>
2023-02-12 17:54:22 +0100

    feat(business-users-integration) Migrations, context and repository for user data

---
+++ Business/Users/SolveX.Business.Users.Integration/Context/UserContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SolveX.Business.Users.Domain.Models;
+
+namespace SolveX.Business.Users.Integration.Context;
+
+public class UserContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public UserContext(IConfiguration configuration)
+      :base()
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+
+            optionsBuilder.UseSqlite(_configuration.GetConnectionString("Default"));
+        }
+
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+
+    //entities
+    public DbSet<UserModel> Users { get; set; }
+}
---
+++ Business/Users/SolveX.Business.Users.Integration/Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using SolveX.Business.Users.Domain.Models;
+using SolveX.Business.Users.Domain.Repositories;
+using SolveX.Business.Users.Integration.Context;
+using SolveX.Framework.Integration.Repositories;
+
+namespace SolveX.Business.Users.Integration.Repositories;
+public class UserRepository : GenericRepository<UserModel>, IUserRepository
+{
+    public UserRepository(UserContext context):base(context)
+    {
+    }
+}
