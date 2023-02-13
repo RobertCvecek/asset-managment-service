3f31d44e5c1c42206791d32ce196785e2ef9fb19
Robert_Cvecek <robi.cvecek1@gmail.com>
2023-02-13 21:50:19 +0100

    feat(application-asset-managment): Controller for assets and models for requests

---
+++ Application/SolveX.AssetManagment/Models/Requests/CreateAssetRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SolveX.AssetManagment.Models.Requests;
+
+public class CreateAssetRequest
+{
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; }
+
+    [Required]
+    public string Data { get; set; }
+}
