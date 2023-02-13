434ab3bd42fa2bb483c2ee72f3ccfbbcec748dd1
Robert_Cvecek <robi.cvecek1@gmail.com>
2023-02-13 22:55:03 +0100

    feat(business-assets-domain): Method for searching linked assets

--- Business/Assets/SolveX.Business.Assets.Domain/DomainServices/IAssetDomainService.cs
+++ Business/Assets/SolveX.Business.Assets.Domain/DomainServices/IAssetDomainService.cs
@@ -3,11 +3,13 @@
 namespace SolveX.Business.Assets.Domain.DomainServices;
 public interface IAssetDomainService
 {
-    public Task<int> Create(int id, string title, string data);
+    public Task<int> Create(int id, string title, string data, IEnumerable<int> links);
 
     public Task<Asset> Get(int id);
 
     public Task<Asset?> Get(string title);
 
     public Task<Asset?> Get(string atribute, string attributeValue);
+    
+    public Task<IEnumerable<Asset>> GetLinkedAssets(int assetId);
 }
