b34f845cd540f5daaeec73bcababb5afb3fc378d
Robert_Cvecek <robi.cvecek1@gmail.com>
2023-02-13 21:49:54 +0100

    feat(business-asset-api): Methods for getting and creating assets and mappers

---
+++ Business/Assets/SolveX.Business.Assets.ApplicationServices/Services/AssetService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using SolveX.Business.Assets.API.Dtos;
+using SolveX.Business.Assets.API.Services;
+using SolveX.Business.Assets.Domain.DomainServices;
+
+namespace SolveX.Business.Assets.ApplicationServices.Services;
+public class AssetService : IAssetService
+{
+    private readonly IAssetDomainService _assetDomainService;
+    private readonly IMapper _mapper;
+
+    public AssetService(IAssetDomainService assetService, IMapper mapper)
+    {
+        _assetDomainService = assetService;
+        _mapper = mapper;
+    }
+
+    public async Task<int> Create(int id, string title, string data)
+    {
+        return await _assetDomainService.Create(id, title, data);
+    }
+
+    public async Task<AssetDto> Get(int id)
+    {
+        return _mapper.Map<AssetDto>(await _assetDomainService.Get(id));
+    }
+
+    public async Task<AssetDto> Get(string title)
+    {
+        return _mapper.Map<AssetDto>(await _assetDomainService.Get(title));
+    }
+
+    public async Task<AssetDto> Get(string atribute, string attributeValue)
+    {
+        return _mapper.Map<AssetDto>(await _assetDomainService.Get(atribute, attributeValue));
+    }
+}
