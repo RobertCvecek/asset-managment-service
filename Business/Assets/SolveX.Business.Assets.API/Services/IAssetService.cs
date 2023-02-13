cbf2f6a26128f82ea078031fa8215fc1416bb742
Robert_Cvecek <robi.cvecek1@gmail.com>
2023-02-13 21:49:54 +0100

    feat(business-asset-api): Methods for getting and creating assets and mappers

---
+++ Business/Assets/SolveX.Business.Assets.API/Services/IAssetService.cs
@@ -0,0 +1,13 @@
+﻿using SolveX.Business.Assets.API.Dtos;
+
+namespace SolveX.Business.Assets.API.Services;
+public interface IAssetService
+{
+    public Task<int> Create(int id, string title, string data);
+
+    public Task<AssetDto> Get(int id);
+
+    public Task<AssetDto> Get(string title);
+
+    public Task<AssetDto> Get(string atribute, string attributeValue);
+}
---
+++ Business/Assets/SolveX.Business.Assets.ApplicationServices/Mappings/AssetMap.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SolveX.Business.Assets.API.Dtos;
+using SolveX.Business.Assets.Domain.Models;
+
+namespace SolveX.Business.Users.ApplicationServices.Mappings;
+
+public class AssetMap : Profile
+{
+    public AssetMap()
+    {
+        CreateMap<AssetDto, Asset>()
+            .ReverseMap();
+    }
+}
