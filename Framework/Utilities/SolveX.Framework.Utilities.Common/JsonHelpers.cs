ce0d0added648a86b456a0034bab894ecbd33d85
Robert_Cvecek <robi.cvecek1@gmail.com>
2023-02-13 21:47:50 +0100

    feat(framework-utilities-common): Added json helper for searching property values

---
+++ Framework/Utilities/SolveX.Framework.Utilities.Common/JsonHelpers.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace SolveX.Framework.Utilities.Common;
+public static class JsonHelpers
+{
+    public static bool PropertyExistsWithValue(string jsonString, string propertyName, string expectedValue)
+    {
+        JObject json = JObject.Parse(jsonString);
+        return PropertyExistsWithValue(json, propertyName, expectedValue);
+    }
+
+    private static bool PropertyExistsWithValue(JToken token, string propertyName, string expectedValue)
+    {
+        if (token.Type == JTokenType.Object)
+        {
+            JObject obj = (JObject)token;
+            foreach (var property in obj.Properties())
+            {
+                if (property.Name.Equals(propertyName, StringComparison.OrdinalIgnoreCase)
+                    && property.Value.ToString().Equals(expectedValue, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+
+                if (PropertyExistsWithValue(property.Value, propertyName, expectedValue))
+                {
+                    return true;
+                }
+            }
+        }
+        else if (token.Type == JTokenType.Array)
+        {
+            JArray array = (JArray)token;
+            foreach (var item in array.Children())
+            {
+                if (PropertyExistsWithValue(item, propertyName, expectedValue))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
