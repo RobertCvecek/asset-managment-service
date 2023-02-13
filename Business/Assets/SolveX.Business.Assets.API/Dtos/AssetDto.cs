d2397948a70cb626b6fb0d92c20efeeaf325fbe9
Robert_Cvecek <robi.cvecek1@gmail.com>
2023-02-13 21:49:54 +0100

    feat(business-asset-api): Methods for getting and creating assets and mappers

---
+++ Business/Assets/SolveX.Business.Assets.API/Dtos/AssetDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SolveX.Business.Assets.API.Dtos;
+public class AssetDto
+{
+    public string Title { get; set; }
+
+    public int Id { get; set; }
+
+    public string Data { get; set; }
+}
---
+++ Business/Assets/SolveX.Business.Assets.ApplicationServices/AssetApplicationModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using SolveX.Business.Assets.API.Services;
+using SolveX.Business.Assets.ApplicationServices.Services;
+
+namespace SolveX.Business.Users.Integration;
+
+public class AssetApplicationModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<AssetService>().As<IAssetService>();
+    }
+}
