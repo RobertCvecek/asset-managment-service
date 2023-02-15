d3c4b5ab039e5c4979091d3d98a61a9fb08cf599
Robert_Cvecek <robi.cvecek1@gmail.com>
2023-02-15 22:00:51 +0100

    tests(business-assets-integration): Integration tests for asset application service

---
+++ Business/Assets/SolveX.Business.Assets.IntegrationTests/ApplicationServicesTests/AssetServiceTest.cs
@@ -0,0 +1,41 @@
+﻿
+using AutoBogus;
+using Bogus;
+using FluentAssertions;
+using NUnit.Framework;
+using SolveX.Business.Assets.API.Dtos;
+using SolveX.Business.Assets.API.Services;
+using SolveX.Business.Assets.Domain.Models;
+using SolveX.Business.Assets.Integration.Context;
+using SolveX.Business.Assets.IntegrationTests.Generators;
+using SolveX.Framework.TestingUtility.DependencyInjectionTests;
+
+namespace SolveX.Business.Assets.IntegrationTests.ApplicationServices;
+
+public class AssetServiceTest : InjectionBasedTest
+{
+    private IAssetService _service;
+    private AssetTestDataGenerator _generator;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        _service = Resolve<IAssetService>();
+        _generator = new(Resolve<AssetContext>());
+    }
+
+    /// <summary>
+    /// Test if asset service inserts asset
+    /// </summary>
+    /// <returns></returns>
+    [Test]
+    public async Task ShouldGetAssetById()
+    {
+        Asset asset = new AutoFaker<Asset>().Generate();
+        int id = await _generator.Insert(asset);
+
+        AssetDto assetDto = await _service.Get(asset.Id);
+
+        assetDto.Should().BeEquivalentTo(asset);
+    }
+}
---
+++ Business/Assets/SolveX.Business.Assets.IntegrationTests/GlobalSetup.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using SolveX.Business.Assets.ApplicationServices;
+using SolveX.Business.Users.Integration;
+using SolveX.Framework.Integration;
+using SolveX.Framework.TestingUtility.DependencyInjectionTests;
+using System.Reflection;
+using System.Data.SQLite;
+using SolveX.Business.Assets.Integration.Context;
+using Autofac;
+using Microsoft.EntityFrameworkCore;
+
+namespace SolveX.Business.Assets.IntegrationTests;
+
+[SetUpFixture]
+public class GlobalSetup
+{
+    private readonly List<Autofac.Module> modules = new List<Autofac.Module>
+    {
+        new FrameworkIntegrationModule(),
+        new AssetIntegrationModule(),
+        new AssetApplicationModule(),
+        new AssetDomainModule()
+    };
+
+    IConfigurationRoot configuration;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile(
+            path: "appsettings.json",
+            optional: false,
+            reloadOnChange: true)
+        .Build();
+
+        List<Assembly> listOfAssemblies = new();
+        var mainAsm = Assembly.GetEntryAssembly();
+        listOfAssemblies.Add(mainAsm);
+
+        foreach (var refAsmName in Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll")
+            .Select(x => Assembly.Load(AssemblyName.GetAssemblyName(x)))
+            .Where(assembly => assembly.GetName().Name.StartsWith("SolveX.Business.Assets")))
+        {
+            listOfAssemblies.Add(refAsmName);
+        }
+
+        Autofac.IContainer container = InjectionBasedTest.Init(models: modules, configuration: configuration, autoMapperConfigs: listOfAssemblies);
+
+        SetupDatabase(container);
+    }
+
+    private string connectionString => configuration.GetConnectionString("Default");
+
+    public void SetupDatabase(Autofac.IContainer container)
+    {
+        ///Create database
+        SQLiteConnection.CreateFile(connectionString.Replace("Data Source=", ""));
+        var assetContext = container.Resolve<AssetContext>();
+        assetContext.Database.Migrate();
+    }
+
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        FileInfo fi = new FileInfo(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), connectionString.Replace("Data Source=", "")));
+        if (fi.Exists ) 
+        {
+            SQLiteConnection connection = new SQLiteConnection(connectionString);
+            connection.Close();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            fi.Delete();
+        }
+    }
+}
