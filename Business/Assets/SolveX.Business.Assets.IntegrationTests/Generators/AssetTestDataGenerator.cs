244359ab9fea1d349308f1095ba09771876f97c5
Robert_Cvecek <robi.cvecek1@gmail.com>
2023-02-15 22:00:51 +0100

    tests(business-assets-integration): Integration tests for asset application service

---
+++ Business/Assets/SolveX.Business.Assets.IntegrationTests/Generators/AssetTestDataGenerator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using SolveX.Business.Assets.Domain.Models;
+using SolveX.Business.Assets.Integration.Context;
+
+namespace SolveX.Business.Assets.IntegrationTests.Generators;
+public class AssetTestDataGenerator
+{
+    public AssetContext _context;
+    public AssetTestDataGenerator(AssetContext context)
+    {
+        _context = context;
+    }
+    /// <summary>
+    /// Inserts asset
+    /// </summary>
+    /// <returns>Id of asset</returns>
+    public async Task<int> Insert(Asset asset)
+    {
+        _context.Set<Asset>().Add(asset);
+        return await _context.SaveChangesAsync();
+    }
+
+}
